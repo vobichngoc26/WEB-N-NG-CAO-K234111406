ce423064bdf6758c4c574e8cf4d6256219267d79
vobichngoc26 <ngocvb234111e@st.uel.edu.vn>
2026-01-25 01:53:08 +0700

    Add files via upload

---
+++ exercise 18/app-module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing-module';
+
+import { App } from './app';
+import { About } from './about/about';
+import { Notfound } from './notfound/notfound';
+import { Listproduct } from './listproduct/listproduct';
+import { Productdetail } from './productdetail/productdetail';
+import { ServiceProductImageEvent } from './service-product-image-event/service-product-image-event';
+import { ProductService } from './product-service';
+import { ProductComponent } from './product-component/product-component';
+import { Listcustomers } from './listcustomers/listcustomers';
+
+@NgModule({
+  declarations: [App, About, Notfound, Listproduct, Productdetail, ServiceProductImageEvent, ProductComponent, Listcustomers],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [ProductService],
+  bootstrap: [App]
+})
+export class AppModule {}
---
+++ exercise 18/app-routing-module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { About } from './about/about';
+import { Listcustomer2 } from './listcustomer2/listcustomer2';
+import { Listcustomerba } from './listcustomerba/listcustomerba';
+import { Listcustomer } from './listcustomer/listcustomer';
+import { Listproduct } from './listproduct/listproduct';
+import { Productdetail } from './productdetail/productdetail';
+import { Notfound } from './notfound/notfound';
+import { ServiceProductImageEventDetail } from './service-product-image-event-detail/service-product-image-event-detail';
+import { ServiceProductImageEvent } from './service-product-image-event/service-product-image-event';
+import { ProductComponent } from './product-component/product-component';
+
+const routes: Routes = [
+  { path: 'gioi-thieu', component: About },
+  //{path:"khach-hang-1",component: Listcustomer},
+  //{path:"khach-hang-2",component: Listcustomer2},
+  //{path:"khach-hang-3",component: Listcustomerba},
+  //{path:"san-pham-1",component: Listproduct},
+  //{path:"san-pham-1/:id",component: Productdetail},
+  //{path:'service-product-image-event', component:ServiceProductImageEvent},
+  //{path:'service-product-image-event/:ProductId',component:ServiceProductImageEventDetail},
+  //{path:'product',component:ProductComponent},
+  //{path:'service-product',component:ServiceProductImageEvent},
+  //{path:"**",component: Notfound},
+  
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+export const RoutingComponent=[
+ProductComponent,
+Listproduct,
+ServiceProductImageEvent,
+]
